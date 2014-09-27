a6dc6048769e3c22f1870b5624edd88f34099270
Franco Catena <francocatena@gmail.com>
2014-09-26 23:56:25 -0300

    Compass mixins added

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  name: 'francocatena:compass',
+  summary: 'Stylesheet Authoring Environment that makes your website design simpler to implement and easier to maintain',
+  version: '0.1.0',
+  git: 'https://github.com/francocatena/meteor-compass'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@0.9.3')
+})
