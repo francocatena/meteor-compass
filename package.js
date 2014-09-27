b1f753c0d6f2e59ced037a7040cdea1a2b27fa4a
Franco Catena <francocatena@gmail.com>
2014-09-27 01:53:20 -0300

    Move sass_options to scss

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'francocatena:compass',
   summary: 'Stylesheet Authoring Environment that makes your website design simpler',
-  version: '0.4.1',
+  version: '0.4.2',
   git: 'https://github.com/francocatena/meteor-compass'
 });
 
