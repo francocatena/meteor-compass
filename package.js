c00c1ee7ffe261c6f761daceca0e71f52006ba34
Franco Catena <francocatena@gmail.com>
2014-09-27 19:14:01 -0300

    Version 0.5.1 bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'francocatena:compass',
   summary: 'Stylesheet Authoring Environment that makes your website design simpler',
-  version: '0.5.0',
+  version: '0.5.1',
   git: 'https://github.com/francocatena/meteor-compass'
 });
 
