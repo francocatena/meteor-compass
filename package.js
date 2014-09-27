80710715f1903bb76480cdcb660527536aa5bbc9
Franco Catena <francocatena@gmail.com>
2014-09-27 01:55:52 -0300

    Add only scss.json to package.js

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'francocatena:compass',
   summary: 'Stylesheet Authoring Environment that makes your website design simpler',
-  version: '0.4.2',
+  version: '0.4.3',
   git: 'https://github.com/francocatena/meteor-compass'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.3')
 
-  api.addFiles('sass_include_paths.json', 'server')
+  api.addFiles('scss.json', 'server')
 })
